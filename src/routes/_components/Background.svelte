c6448992d40f387bf2019dc0956cf2eb1b3a048e
Sean Catangui <seancaat@gmail.com>
2024-06-19 22:37:04 -0400

    safari fix?

--- src/routes/_components/Background.svelte
+++ src/routes/_components/Background.svelte
@@ -12,14 +12,14 @@
     top: 0;
     left: 0;
     right: 0;
-    bottom: 0;
+    height: 100dvh;
     z-index: 0;
   }
   
   .topper-image {
     position: absolute;
     width: 100%;
-    outline: 4px dashed red;
+    /* outline: 4px dashed red; */
     height: 100%;
     object-fit: cover;
     -webkit-user-drag: none;
