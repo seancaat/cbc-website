a2fce5c521c4b1eccedd0469e926c327f05e8f8e
Sean Catangui <seancaat@gmail.com>
2024-06-25 19:11:48 -0400

    home tab label

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,7 +2,7 @@ import { env } from '$env/dynamic/private';
 export async function load({fetch}) {
 
   const forecast_response = await fetch("https://api.weather.gov/gridpoints/OKX/33,35/forecast");
-  const weather_data = await forecast_response.json();
+  const weather_data = await forecast_response.json()
   const sunday_weather = weather_data.properties.periods.find(p => p.name === "Sunday") || weather_data.properties.periods.find(p => p.name === "Today");
 
   return sunday_weather;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Chinatown Basketball Club</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
